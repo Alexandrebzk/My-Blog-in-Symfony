cafd9150f65b046001d7cba806fc382837802bd7
Alex <alexandre.barczyk@ig2i.centralelille.fr>
2021-03-26 10:33:49 +0100

    Twig part

--- src/Controller/LuckyNumberController.php
+++ src/Controller/LuckyNumberController.php
@@ -9,13 +9,17 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class LuckyNumberController extends AbstractController
 {
-    /**
-     * @Route("/lucky_number", name="app_lucky_number")
-     * @param Request $request
-     * @return Response
-     */
+    #[Route('/lucky_number', name: 'luckyNumber')]
     public function index(Request $request): Response
     {
+        /**
+         *
+        $number = random_int(0, 100);
+
+        return new Response(
+        "<p>Lucky number: $number</p>"
+        );
+         */
         $name = $request->query->get('name');
         return $this->json([
             'message' => 'Happy to see you M. or Mrs. ' . $name
